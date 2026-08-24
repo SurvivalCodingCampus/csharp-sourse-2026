f8929071a019fa5faf65d3ee73d66e3e5cffc65a
yoooooungwoon <kim0310hwang@gmail.com>
2026-08-24 15:24:35 +0900

    feat:0824과제제출

---
+++ ClericTest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿namespace ClericTest;
+using Day01_OOP_Review;
+
+public class UnitTest1
+{
+    [Fact]
+    public void SelfAid_테스트()
+    {
+        // Given
+        Cleric cleric = new Cleric("김용운");
+        cleric.Hp = 10;
+
+        // When
+        cleric.SelfAid();
+
+        // Then
+        Assert.Equal(Cleric.MaxHp, cleric.Hp);
+        Assert.Equal(5, cleric.Mp);
+    }
+    [Fact]
+    public void Pray_최대마나초과_테스트()
+    {
+        // Given
+        Cleric cleric = new Cleric("김용운");
+        cleric.Mp = 8;
+
+        // When (10초 기도)
+        int recovered = cleric.Pray(10);
+
+        // Then (8 -> 10이 되었으므로 실제 회복량은 2여야 함)
+        Assert.Equal(2, recovered);
+        Assert.Equal(Cleric.MaxMp, cleric.Mp);
+    }
+}
