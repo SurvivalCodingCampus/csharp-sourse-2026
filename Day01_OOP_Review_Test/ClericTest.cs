e33588d4604b38383449fc0763299805c16581a9
Jaewon.Moon <mjw9313@gmail.com>
2026-08-24 16:17:29 +0900

    fix.Test를 같이 commit 못해서 재 commit

---
+++ Day01_OOP_Review_Test/ClericTest.cs
@@ -0,0 +1,52 @@
+﻿using Day01_OOP_Review;
+
+namespace Day01_OOP_Review_Test;
+
+public class ClericTest
+{
+    private Cleric _cleric;
+    private const int PraySec = 2;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _cleric = new Cleric("Hero");
+    }
+
+    [Test]
+    public void NameTest()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            Cleric cleric2 = new Cleric(null);
+        });
+        
+        Assert.That(_cleric.Name, Is.EqualTo("Hero"));
+    }
+
+    [Test]
+    public void SelfAidTest()
+    {
+        _cleric.SelfAid();
+        Assert.That(_cleric.Mp, Is.EqualTo(5));
+        
+        _cleric.SelfAid();
+        Assert.That(_cleric.Mp, Is.EqualTo(0));
+        
+        _cleric.SelfAid();
+        Assert.That(_cleric.Mp, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void PrayTest()
+    {
+        _cleric.SelfAid();
+        Assert.That(_cleric.Mp, Is.EqualTo(5));
+        
+        Assert.That(_cleric.Pray(PraySec), Is.InRange(2,4));
+
+        int beforeMp = _cleric.Mp;
+        int recoverMp = _cleric.Pray(PraySec);
+        Assert.That(_cleric.Mp, Is.EqualTo(beforeMp + recoverMp));
+    }
+}
