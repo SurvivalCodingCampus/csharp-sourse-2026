cdeab962981336d39292186b49ddb72778ce8216
hlee971001-rgb <hlee971001@gmail.com>
2026-08-24 16:37:19 +0900

    0824_과제

---
+++ Day01_OOP_Review_Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Day01_OOP_Review_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
