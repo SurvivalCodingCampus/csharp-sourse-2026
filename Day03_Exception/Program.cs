c0748d7b36ca19e75d70d69555828d92571a1107
hlee971001-rgb <hlee971001@gmail.com>
2026-08-31 16:50:41 +0900

    예외_연습문제_1_2

---
+++ Day03_Exception/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day03_Exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numString = "10.5";
+        int num;
+        try
+        {
+            num = int.Parse(numString);
+        }
+        catch (Exception e)
+        {
+            num = 0;
+        }
+        
+        Console.WriteLine(num);
+    }
+}
