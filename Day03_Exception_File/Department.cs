9d614b54e01ab707071c1f33c43b52995e8f4ed2
yoooooungwoon <kim0310hwang@gmail.com>
2026-08-31 16:42:22 +0900

    feat: 2026-08-31 과제제출

---
+++ Day03_Exception_File/Department.cs
@@ -0,0 +1,7 @@
+namespace Day03_Exception_File;
+
+public class Department(string name, Employee leader)
+{ 
+    public string Name { get; } = name;
+    public Employee leader { get; } = leader;
+}
