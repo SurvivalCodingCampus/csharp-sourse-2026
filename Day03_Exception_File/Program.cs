8994bd9f7866be2c58a1e44216f52a8fd5eafd95
Sungyeon.Park <sypdana@gmail.com>
2026-08-31 16:54:25 +0900

    260831_예외 과제

---
+++ Day03_Exception_File/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace Day03_Exception_File;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //예외 연습문제 1
+        var numString = "10.5";
+        int num = int.Parse(numString);
+        //Console.WriteLine(num);
+
+        try
+        {
+            Console.WriteLine(num);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        //예외 연습문제 2
+        try
+        {
+            Console.WriteLine(num);
+        }
+        catch (Exception e)
+        {
+            num = 0;
+            Console.WriteLine(num);
+        }
+        
+       
+       
+    }
+}
