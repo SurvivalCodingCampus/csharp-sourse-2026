0bb8d07004ef2ffa7f9cbc966b6255ede2d9e146
hlee971001-rgb <hlee971001@gmail.com>
2026-08-31 17:36:25 +0900

    여러가지 데이터 형식_연습문제

---
+++ Day03_Exception/Serialization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Day03_Exception;
+
+class MainProgram
+{
+    static void OldMain(string[] args)
+    {
+       
+        Employee leader = new Employee("홍길동", 41);
+        Department department = new Department("총무부", leader);
+
+        var options = new JsonSerializerOptions 
+        { 
+            WriteIndented = true 
+        };
+        
+        string jsonString = JsonSerializer.Serialize(department, options);
+        
+        File.WriteAllText("company.json", jsonString);
+
+        Console.WriteLine("company.json 파일 저장 완료!");
+        Console.WriteLine(jsonString);
+    }
+}
