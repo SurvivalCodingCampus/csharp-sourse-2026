4691fccc194447dc33aa15ab95b3da205fcb9ecf
KJY <dbvcxz@gmail.com>
2026-08-31 13:07:38 +0900

    예외 연습문제 1

---
+++ Day03_Exception/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day03_Exception;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numString = "10.5";
+        int num = int.Parse(numString);
+        Console.WriteLine(num);
+        //  at System.Number.ThrowFormatException[TChar](ReadOnlySpan`1 value)
+        
+    }
+}
