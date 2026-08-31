5f3d5e9e74d5fe1bc47bffd6cac706fcc0da3551
KJY <dbvcxz@gmail.com>
2026-08-31 13:39:39 +0900

    파일 조작 연습문제

---
+++ Day03_File/IFileCopier.cs
@@ -0,0 +1,6 @@
+﻿namespace Day03_File;
+
+public interface IFileCopier
+{
+    void CopyFile(string sourceFilePath, string destinationFilePath);
+}
---
+++ Day03_File/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day03_File;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcePath = args[0];
+        string destPath = args[1];
+
+        IFileCopier fileCopier = new DefaultFileCopier();
+        fileCopier.CopyFile(sourcePath, destPath);
+    }
+}
