bb60b017e9d4fd818ec14e4619f6dd41e800deab
seosuhyeon <giornogiobana1234@gmail.com>
2026-08-31 16:40:59 +0900

    260831_18_서수현_1

--- Day03_Review_Test/DepartmentTest.cs
+++ Day03_Review_Test/DepartmentTest.cs
@@ -17,19 +17,13 @@ public class DepartmentTest
     public void METHOD()
     {
         // Arrange
-        //9-2
-        string Writing = "Hello World";
-        File.WriteAllText("sourceFilePath.txt", Writing);
+        
+
         
         //9-3
         Employee employee = new Employee("HongGilDong", 41);
         Department department = new Department("Secretary", employee); //총무 번역시 Secretary로 번역
         // Act
-        //9-2
-        DefaultFilecopier defaultFilecopier = new DefaultFilecopier(Writing, "destinationFilePath.txt");
-        byte[] originalBytes = File.ReadAllBytes("sourceFilePath.txt");
-        byte[] copiedBytes = File.ReadAllBytes("destinationFilePath.txt");
-        
         //9-3
         string jsonString = JsonSerializer.Serialize(department);
         using JsonDocument json = JsonDocument.Parse(jsonString);
@@ -40,9 +34,7 @@ public class DepartmentTest
         Department? loadedDepartment = JsonSerializer.Deserialize<Department>(jsonString);
 
         Console.WriteLine("Act 성공시 출력");
-        //9-2
-        // Assert
-        Assert.AreEqual(originalBytes, copiedBytes);
+        
         
         //9-3
         // Assert 1 json 내부 값 검사
@@ -55,5 +47,27 @@ public class DepartmentTest
         Assert.AreEqual("HongGilDong", department.Leader.Name);
         Assert.AreEqual(41, department.Leader.Age);
         Console.WriteLine("Assert 성공시 출력");
+    }
+
+    [Test]
+    public void Test()
+    {
+        // Arrange
+        //9-2
+        string writing = "Hello World";
+        string sourcePath = "sourceFilePath.txt";
+        string destinationPath = "destinationFilePath.txt";
+        
+        File.WriteAllText(sourcePath, writing);
+        // Act
+
+        //9-2
+        DefaultFilecopier defaultFilecopier = new DefaultFilecopier(sourcePath, destinationPath);
+        byte[] originalBytes = File.ReadAllBytes(sourcePath);
+        byte[] copiedBytes = File.ReadAllBytes(destinationPath);
+        
+        //9-2
+        // Assert
+        CollectionAssert.AreEqual(originalBytes, copiedBytes);
     }
 }
