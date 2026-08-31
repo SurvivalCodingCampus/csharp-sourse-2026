8f0176f38c3d755a5fa4ae91f1ee2b42ae712b3e
KJY <dbvcxz@gmail.com>
2026-08-31 13:13:45 +0900

    예외 연습문제 2

--- Day03_Exception/Program.cs
+++ Day03_Exception/Program.cs
@@ -5,7 +5,16 @@ class Program
     static void Main(string[] args)
     {
         var numString = "10.5";
-        int num = int.Parse(numString);
+        int num;
+        try
+        {
+            num = int.Parse(numString);
+        }
+        catch (Exception e)
+        {
+            num = 0;
+        }
+        
         Console.WriteLine(num);
         //  at System.Number.ThrowFormatException[TChar](ReadOnlySpan`1 value)
         
