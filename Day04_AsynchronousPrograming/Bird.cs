f577bb8651b687416998307f642078b32e4a2069
seosuhyeon <giornogiobana1234@gmail.com>
2026-09-01 13:24:44 +0900

    260901_19_서수현_2

--- Day04_AsynchronousPrograming/Bird.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Day04_AsynchronousPrograming;
-
-public class Bird
-{
-    string bird1 = "꾸우";
-    string bird2 = "까악";
-    string bird3 = "짹짹";
-}
--- Day04_AsynchronousPrograming/Program.cs
+++ Day04_AsynchronousPrograming/Program.cs
@@ -7,5 +7,20 @@ class Program
         string bird1 = "꾸우";
         string bird2 = "까악";
         string bird3 = "짹짹";
+        for (int i = 0; i < 4; i++)
+        {
+            Task.Delay(1000).Wait();
+            Console.WriteLine(bird1);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            Task.Delay(2000).Wait();
+            Console.WriteLine(bird2);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            Task.Delay(3000).Wait();
+            Console.WriteLine(bird3);
+        }
     }
 }
