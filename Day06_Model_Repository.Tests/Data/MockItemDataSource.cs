28d9b51435398ec2ec538cfd55b73f6bfc24e590
hlee971001-rgb <hlee971001@gmail.com>
2026-09-08 19:36:43 +0900

    feat: Day06_Model_Repository

---
+++ Day06_Model_Repository.Tests/Data/MockItemDataSource.cs
@@ -0,0 +1,20 @@
+﻿using Day06_Model_Repository.Models;
+using Day06_Model_Repository.DataSources;
+
+namespace Day06_Model_Repository.Tests.Data;
+
+public class MockItemDataSource : IItemDataSource
+{
+    public List<Item> Items { get; set; } = new List<Item>();
+
+    public Task<List<Item>> LoadAllItemsAsync()
+    {
+        return Task.FromResult(Items);
+    }
+
+    public Task SaveAllItemsAsync(List<Item> items)
+    {
+        Items = items;
+        return Task.CompletedTask;
+    }
+}
---
+++ Day06_Model_Repository/Data/IInventoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Day06_Model_Repository.Models;
+
+namespace Day06_Model_Repository.Data;
+
+public interface IInventoryRepository
+{
+    public Task<List<Item>> GetItemsAsync();
+    public Task<Item?> GetItemByIdAsync(int itemId);
+    public Task<bool> AddItemAsync(Item item);
+}
