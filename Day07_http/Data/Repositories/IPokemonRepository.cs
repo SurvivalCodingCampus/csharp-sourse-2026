8a581bd1ebedfe3ce5ca1ad7965aee077a4315b0
Sungyeon.Park <sypdana@gmail.com>
2026-09-14 16:58:30 +0900

    260914_http 초안_참고출처작성

---
+++ Day07_http/Data/Repositories/IPokemonRepository.cs
@@ -0,0 +1,14 @@
+﻿using Day07_http.Models;
+
+namespace Day07_http.Data.Repositories;
+
+public interface IPokemonRepository
+{
+    // C# 14. 네트워크 통신 p.39 참조 
+    Task<List<Pokemon>> GetPokemonsAsync();
+    
+    Task<Pokemon?> GetPokemonBynameAsync(string name);
+    Task<Pokemon?> CreatePokemonAsync(Pokemon pokemon);
+    Task<bool> UpdatePokemonAsync(Pokemon pokemon);
+    Task<bool> DeletePokemonAsync(string name);
+}
---
+++ Day07_http/Models/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Day07_http.Models;
+
+public class Pokemon
+{
+    [JsonProperty("name")] //어트리뷰트 변경
+    public string? Name { get; set; }
+    
+    [JsonPropertyName("sprites")] // 어트리뷰트 변경
+    public OtherSprites? Sprites { get; set; }
+}
+
+public class JsonPropertyAttribute : Attribute
+{
+    public JsonPropertyAttribute(string name)
+    {
+        throw new NotImplementedException();
+    }
+}
